38fe4f9c26b4f96aed96332b8ea1dd0ab93335e0
wzyoung <wzyoung@163.com>
2015-12-13 15:06:02 +0800

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by wzyoung on 2015/12/6.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        String url = "http://de0.cc/J2j23j";
+
+        System.out.println(url.substring(url.lastIndexOf("/") + 1, url.length()));
+    }
+
+
+}
